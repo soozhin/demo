7921328d95be57b48631aaee67778db21dcbda2f
soozhin <53286620+soozhin@users.noreply.github.com>
2024-05-25 14:37:59 +0900

    made hello page

--- src/main/java/com/soundboard/demo/DemoApplication.java
+++ src/main/java/com/soundboard/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.soundboard.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.soundboard.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/soundboard/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.soundboard.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "/hello/index";
+    }
+}
